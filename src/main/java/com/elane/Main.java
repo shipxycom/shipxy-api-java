030305531b109673c3e1cb5a6b6f6f57f945c30b
White <249898979@qq.com>
2026-08-04 16:42:38 +0800

    commit

--- src/main/java/com/elane/Main.java
+++ src/main/java/com/elane/Main.java
@@ -5,8 +5,8 @@ import com.elane.request.*;
 import com.elane.response.*;
 
 public class Main {
-    private static String key = com.elane.api.Key.key;
-//    private static String key = "请从 API控制台 申请";
+//    private static String key = com.elane.api.Key.key;
+    private static String key = "请从 API控制台 申请";
 
     public static void main(String[] args) {
         SearchShipFn();
